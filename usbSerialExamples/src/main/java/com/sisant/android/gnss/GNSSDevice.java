78a7e5b361a7c4a00c12cb4fa18f2da264c19e9d
alonso <acuna007@gmail.com>
2026-08-07 09:33:40 -0600

    .

--- usbSerialExamples/src/main/java/com/sisant/android/gnss/GNSSDevice.java
+++ usbSerialExamples/src/main/java/com/sisant/android/gnss/GNSSDevice.java
@@ -545,7 +545,7 @@ public class GNSSDevice {
         if (this.MAC.equals("00:14:03:05:0A:10")) return CMD_TYPE_UM; //Rover JC8 sin radio
         if (this.MAC.equals("00:14:03:05:06:7A")) return CMD_TYPE_UM; //Rover MVM, la base es vieja
         if (this.MAC.equals("00:14:03:05:0A:23")) return CMD_TYPE_UM; //Rover EOZ
-        if (this.MAC.equals("00:14:03:05:0A:0F") || this.MAC.equals("00:14:03:05:0A:52")) return CMD_TYPE_UM; //WIR
+        if (this.MAC.equals("00:14:03:05:0A:0F") || this.MAC.equals("00:14:03:05:0A:52")) return CMD_TYPE_UM; //WIR, y BASE MC5 tienen 0A:52
         if (this.MAC.equals("00:14:03:05:0A:2D") || this.MAC.equals("00:14:03:05:0A:48")) return CMD_TYPE_UM; //EST
         if(this.name.contains("PG7")) {
             //00:14:03:05:0A:00 también lo tiene Marchena pero no es UM
@@ -560,6 +560,7 @@ public class GNSSDevice {
         if (this.MAC.equalsIgnoreCase("00:14:03:05:08:8F")) return CMD_TYPE_UM; //rover RC7
         if (this.MAC.equalsIgnoreCase("00:14:03:05:59:5A")) return CMD_TYPE_UM; //base RC7
         if (this.MAC.equalsIgnoreCase("00:14:03:05:0A:6B") || this.MAC.equalsIgnoreCase("00:14:03:05:0A:BA")) return CMD_TYPE_UM; //GA2
+        if (this.MAC.equals("00:14:03:05:0A:3A")) return CMD_TYPE_UM; //rover Mc5
 
 
 
@@ -614,7 +615,7 @@ public class GNSSDevice {
         if (this.MAC.equals("00:14:03:05:06:71")) return true; //BASE Ga9
         if (this.MAC.equals("00:14:03:05:06:6D")) return true; //BASE YTH
         if (this.MAC.equals("00:14:03:05:06:7E")) return true; //BASE MVM
-        if (this.MAC.equals("00:14:03:05:0A:52")) return true; //BASE WIR
+        if (this.MAC.equals("00:14:03:05:0A:52")) return true; //BASE WIR y BASE MC5
         if (this.MAC.equals("00:14:03:05:0A:2D")) return true; //BASE EST
         if (this.MAC.equals("00:14:03:05:0A:0A")) return true; //BASE PG7
         if (this.MAC.equals("00:14:03:05:0A:EB")) return true; //BASE LAM
@@ -622,6 +623,7 @@ public class GNSSDevice {
         if (this.MAC.equals("98:D3:31:F4:B6:62")) return true; //BASE 8M
         if (this.MAC.equals("00:14:03:05:0A:6B")) return true; //BASE GA2
         if (this.MAC.equals("00:14:03:05:59:5A")) return true; //BASE rc7
+
 
 
        // if (this.MAC.equals("98:D3:71:FE:9D:EF")) return true; // rprueba
@@ -636,15 +638,15 @@ public class GNSSDevice {
         //OJO el mes es 0 para enero
         //Base PBX negra
         if (this.MAC.equals("00:14:03:05:0D:83"))
-            (calendar = Calendar.getInstance()).set(2026, ago, 26, 0, 0, 0);
+            (calendar = Calendar.getInstance()).set(2026, set, 26, 0, 0, 0);
 
         //ROVER UM01 PX this.MAC.equals("00:14:03:05:0D:CA". OJO no es UM, es ublox
         if (this.MAC.equals("00:14:03:05:0D:CA"))
-             (calendar = Calendar.getInstance()).set(2026, ago, 26, 0, 0, 0);
+             (calendar = Calendar.getInstance()).set(2026, set, 26, 0, 0, 0);
         if (this.MAC.equals("00:14:03:05:0A:C2"))
-            (calendar = Calendar.getInstance()).set(2026, ago, 26, 0, 0, 0);//BASE APX1
+            (calendar = Calendar.getInstance()).set(2026, set, 26, 0, 0, 0);//BASE APX1
         if (this.MAC.equals("00:14:03:05:0A:D8"))
-            (calendar = Calendar.getInstance()).set(2026, ago, 26, 0, 0, 0);//ROVER APX1
+            (calendar = Calendar.getInstance()).set(2026, set, 26, 0, 0, 0);//ROVER APX1
 
         //CLIENTES CREDITO PENDIENTES ///////////////////
 
@@ -656,7 +658,11 @@ public class GNSSDevice {
 
         ////CRDITOS AL DIA
 
-
+        //MC5
+        if(this.name.contains("MC5")) {
+            if (this.MAC.equals("00:14:03:05:0A:52") || this.MAC.equals("00:14:03:05:0A:3A"))
+              (calendar = Calendar.getInstance()).set(2026, set, 16, 0, 0, 0);
+        }
 
 
 
@@ -683,8 +689,10 @@ public class GNSSDevice {
         }
 
         //WIR william rodriguez
-        if (this.MAC.equals("00:14:03:05:0A:0F") || this.MAC.equals("00:14:03:05:0A:52"))
-            (calendar = Calendar.getInstance()).set(2056, jun, 16, 0, 0, 0);
+        if(this.name.contains("WIR")) { //el 0A:52 lo tiene MC5
+            if (this.MAC.equals("00:14:03:05:0A:0F") || this.MAC.equals("00:14:03:05:0A:52"))
+                (calendar = Calendar.getInstance()).set(2056, jun, 16, 0, 0, 0);
+        }
 
 
         if (this.MAC.equals("00:14:03:05:06:80")) //Base GM3        //Guillermo morales
